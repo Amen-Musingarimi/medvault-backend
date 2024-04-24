16b6c87886fbd71a0d1a6a8a9ec3664eff1d1fb5
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-04-24 16:16:28 +0200

    Add cors for the frontent

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ const app = express();
 // app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json()); //application/json
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use('/patients', patientsRoutes);
 
 app.listen(8080);
