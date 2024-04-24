bb8334880a34924db56be2c786aaafc753a1b23b
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-04-24 14:56:22 +0200

    Create patients routes

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const patientsRoutes = require('./routes/patients');
 
 const app = express();
+
+// app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json()); //application/json
+
+app.use('/patients', patientsRoutes);
 
 app.listen(8080);
