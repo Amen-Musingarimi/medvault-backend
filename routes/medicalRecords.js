3560f82a996ba1de3b92398a0a6aca7ec4fb4c8f
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-04-30 18:08:17 +0200

    Add validations to medical records routes

--- routes/medicalRecords.js
+++ routes/medicalRecords.js
@@ -1,9 +1,62 @@
 const express = require('express');
-const router = express.Router();
+const { body } = require('express-validator');
 const medicalRecordController = require('../controllers/medicalRecords');
 
-// GET /medicalRecords/post;
-router.post('/post', medicalRecordController.createMedicalRecord);
+const router = express.Router();
+
+// POST /medicalRecords/post;
+router.post(
+  '/post',
+  [
+    body('patient').notEmpty(),
+    body('temperature')
+      .notEmpty()
+      .isNumeric(),
+    body('bloodPressure')
+      .notEmpty()
+      .isString(),
+    body('weight')
+      .notEmpty()
+      .isNumeric(),
+    body('reasonForVisit')
+      .notEmpty()
+      .isString(),
+    body('currentSymptoms')
+      .notEmpty()
+      .isString(),
+    body('pastMedicalHistory')
+      .optional()
+      .isString(),
+    body('previousTestResults')
+      .optional()
+      .isString(),
+    body('currentMedications')
+      .optional()
+      .isString(),
+    body('lifestyleAndHabits')
+      .optional()
+      .isString(),
+    body('familyMedicalHistory')
+      .optional()
+      .isString(),
+    body('diagnosis')
+      .notEmpty()
+      .isString(),
+    body('prescription')
+      .notEmpty()
+      .isString(),
+    body('recommendedTests')
+      .optional()
+      .isString(),
+    body('referral')
+      .optional()
+      .isString(),
+    body('checkupDate')
+      .optional()
+      .isDate(),
+  ],
+  medicalRecordController.createMedicalRecord
+);
 
 // GET /medicalRecords/patientId;
 router.get('/:patientId', medicalRecordController.getMedicalRecordsByPatientId);
