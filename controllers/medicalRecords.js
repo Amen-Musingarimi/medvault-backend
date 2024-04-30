7bb7decd3a426af7f7eb39d15ab2de58550abb96
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-04-30 18:38:03 +0200

    Add create medical record controller

--- controllers/medicalRecords.js
+++ controllers/medicalRecords.js
@@ -1,19 +1,70 @@
+const { validationResult } = require('express-validator');
 const MedicalRecord = require('../models/medicalRecord');
 
 // Create a new medical record
-exports.createMedicalRecord = async (req, res) => {
-  try {
-    const medicalRecord = new MedicalRecord(req.body);
-    await medicalRecord.save();
-    res
-      .status(201)
-      .json({
-        message: 'Medical record created successfully',
-        data: medicalRecord,
-      });
-  } catch (error) {
-    res.status(500).json({ error: 'Internal server error' });
+exports.createMedicalRecord = (req, res, next) => {
+  const errors = validationResult(req);
+
+  // !errors.isEmpty means that we have errors
+  if (!errors.isEmpty()) {
+    const error = new Error('Validation failed. Entered data is inorrect.');
+    error.statusCode = 422;
+    throw error;
   }
+
+  const {
+    patient,
+    temperature,
+    bloodPressure,
+    weight,
+    reasonForVisit,
+    currentSymptoms,
+    pastMedicalHistory,
+    previousTestResults,
+    currentMedications,
+    lifestyleAndHabits,
+    familyMedicalHistory,
+    diagnosis,
+    prescription,
+    recommendedTests,
+    referral,
+    checkupDate,
+  } = req.body;
+
+  const medicalRecord = new MedicalRecord({
+    patientID: patient,
+    temperature: temperature,
+    bloodPressure: bloodPressure,
+    weight: weight,
+    reasonForVisit: reasonForVisit,
+    currentSymptoms: currentSymptoms,
+    pastMedicalHistory: pastMedicalHistory,
+    previousTestResults: previousTestResults,
+    currentMedications: currentMedications,
+    lifestyleAndHabits: lifestyleAndHabits,
+    familyMedicalHistory: familyMedicalHistory,
+    diagnosis: diagnosis,
+    prescription: prescription,
+    recommendedTests: recommendedTests,
+    referral: referral,
+    checkupDate: checkupDate,
+  });
+
+  medicalRecord
+    .save()
+    .then((result) => {
+      console.log(result);
+      res.status(201).json({
+        message: 'Medical Record created successfully',
+        medicalRecord: result,
+      });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
 
 // Fetch medical records by patient ID
