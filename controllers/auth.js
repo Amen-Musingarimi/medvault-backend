033c89bcc0b3b026562062475f5ef050c85be9e1
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-05-23 21:22:15 +0200

    Add signup logic

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const { validationResult } = require('express-validator');
 const Doctor = require('../models/doctor');
+const bcrypt = require('bcryptjs');
 
 exports.signup = (req, res, next) => {
   const errors = validationResult(req);
@@ -22,4 +23,32 @@ exports.signup = (req, res, next) => {
     medicalLicenseNumber,
     hospital,
   } = req.body;
+
+  bcrypt
+    .hash(password, 12)
+    .then((hashedPassword) => {
+      const doctor = new Doctor({
+        firstName: firstName,
+        lastName: lastName,
+        emailAddress: emailAddress,
+        password: hashedPassword,
+        gender: gender,
+        phoneNumber: phoneNumber,
+        role: 'doctor',
+        specialization: specialization,
+        medicalLicenseNumber: medicalLicenseNumber,
+        hospital: hospital,
+      });
+
+      return doctor.save();
+    })
+    .then((result) => {
+      res.status(201).json({ message: 'Doctor created!', doctor: result });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
